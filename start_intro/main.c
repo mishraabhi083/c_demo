f5063830968b27411fb5104f799241bdf42229c0
mishraabhi083 <abhimishra847@gmail.com>
2022-03-09 10:17:45 +0530

    day1_shift1

---
+++ start_intro/main.c
@@ -0,0 +1,61 @@
+// headers
+#include<stdio.h>
+
+// custom headers
+
+// declarations
+// global
+int num=0; //identifiers
+
+// main function with arguments 
+// int main(int argc , char** argv){
+//     printf("%d\n",argc);
+
+//     for(int i=0;i<argc;i++){
+//         printf("%s , ",argv[i]);
+//     }
+// }
+
+
+// switch construct
+
+// int main(){
+//     // for loop
+//     // while loop
+//     // do while
+
+//     int num=1;
+
+//     switch (num)
+//     {
+//     case 1:
+//         // do stuff
+//         printf("num is one \n");
+//         break;
+//     case 2:
+//         printf("num is two \n");
+    
+//     default:
+//         break;
+//     }
+
+
+// }
+
+// functions
+
+
+int add(int x , int y); // func declaration
+
+int main(){
+
+    printf("%d",add(5,6));
+    // function call
+}
+
+// function define
+// def 
+int add(int x , int y){
+    // logic
+    return x+y;
+}
